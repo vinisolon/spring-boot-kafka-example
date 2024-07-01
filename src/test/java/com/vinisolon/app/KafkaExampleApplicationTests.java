5c33539c69f650cf9491e12b1019a9c2e500f6d6
Vinicius Solon <vini.97@live.com>
2024-07-01 19:21:42 -0300

    Initial commit

---
+++ src/test/java/com/vinisolon/app/KafkaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinisolon.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
