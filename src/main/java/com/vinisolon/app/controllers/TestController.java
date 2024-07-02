7a7e88aa1a7aa2f4c9402149894eaee0de10f221
Vinicius Solon <vini.97@live.com>
2024-07-01 21:40:21 -0300

    Implemented rest api to call producer

---
+++ src/main/java/com/vinisolon/app/controllers/TestController.java
@@ -0,0 +1,33 @@
+package com.vinisolon.app.controllers;
+
+import com.vinisolon.app.dtos.TestObjectRequest;
+import com.vinisolon.app.producers.KafkaProducer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/kafka")
+public class TestController {
+
+    private final KafkaProducer kafkaProducer;
+
+    public TestController(KafkaProducer kafkaProducer) {
+        this.kafkaProducer = kafkaProducer;
+    }
+
+    @Value("${kafka.test.topic}")
+    private String kafkaTestTopic;
+
+    @PostMapping("/publish")
+    public ResponseEntity<?> publish(@RequestBody TestObjectRequest request) {
+        kafkaProducer.publish(request, kafkaTestTopic);
+        return ResponseEntity.ok("Request body sent to kafka producer");
+    }
+
+}
---
+++ src/main/java/com/vinisolon/app/dtos/TestObjectRequest.java
@@ -0,0 +1,11 @@
+package com.vinisolon.app.dtos;
+
+import lombok.Data;
+
+@Data
+public class TestObjectRequest {
+
+    private Long id;
+    private String message;
+
+}
